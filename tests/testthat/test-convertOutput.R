27ba98f1af89a26af638d234e1308df3a111bc94
masalmon <maelle.salmon@yahoo.se>
2016-02-24 10:31:55 +0100

    why would this fail on Travis?

--- tests/testthat/test-convertOutput.R
+++ tests/testthat/test-convertOutput.R
@@ -1,11 +1,12 @@
 library("ammon")
+library("dplyr")
 #################################################################################################
 context("Convert Output")
 #################################################################################################
 
 test_that("convertOutput works for the CHAI version of output", {
   skip_on_cran()
-  skip_on_travis()
+  #skip_on_travis()
   expect_that(convertOutput(system.file("extdata", "dummyCHAI.csv", package = "ammon"),
                             version="CHAI"), is_a("MicroPEM"))
 
@@ -13,7 +14,7 @@ test_that("convertOutput works for the CHAI version of output", {
 
 test_that("convertOutput works for the Columbia version of output", {
   skip_on_cran()
-  skip_on_travis()
+  #skip_on_travis()
   expect_that(MicroPEMExample <- convertOutput(system.file("extdata", "dummyColumbia.csv", package = "ammon"),
                                                version="Columbia1"), is_a("MicroPEM"))
 
@@ -21,7 +22,7 @@ test_that("convertOutput works for the Columbia version of output", {
 
 test_that("convertOutput works for the Columbia2 version of output", {
   skip_on_cran()
-  skip_on_travis()
+  #skip_on_travis()
   expect_that(convertOutput(system.file("extdata", "dummyColumbia2.csv", package = "ammon"),
                             version="Columbia2"), is_a("MicroPEM"))
 
