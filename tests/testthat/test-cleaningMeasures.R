7e4f1b3103e789a90048a6a12cca17de26a52e9a
Maëlle Salmon <maelle.salmon@yahoo.se>
2016-05-18 17:20:50 +0200

    erases useless test

--- tests/testthat/test-cleaningMeasures.R
+++ tests/testthat/test-cleaningMeasures.R
@@ -11,12 +11,3 @@ test_that("cleaningMeasures outputs a MicroPEM object",{
                                         hepaStart = TRUE, hepaEnd = TRUE)
   expect_that(cleanDataMicroPEM, is_a("MicroPEM"))
 })
-
-test_that("cleaningMeasures also works for MicroPEM that were truncated",{
-  data("dummyMicroPEMChai")
-  shorterMicroPEM <- filterTimeDate(MicroPEMObject=dummyMicroPEMChai,untilTime=NULL,
-                                    fromTime=min(dummyMicroPEMChai$measures$timeDate, na.rm=TRUE) +
-                                      hours(12))
-  cleanDataMicroPEM <- cleaningMeasures(MicroPEMObject=shorterMicroPEM)
-  expect_that(cleanDataMicroPEM, is_a("MicroPEM"))
-})
