87cc7967fe8a9efbf17f27c2820b49dcc4588267
masalmon <maelle.salmon@yahoo.se>
2016-07-07 14:47:10 +0200

    fix?

---
+++ R/micropem.R
@@ -0,0 +1,7 @@
+#' Supports the analyses of RTI MicroPEM output files.
+#'
+#' micropem allows you to read and analyse data from MicroPEM output files
+#' in a reproducible way.
+#'
+"_PACKAGE"
+# > [1] '_PACKAGE'
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 library(testthat)
-library(ammon)
+library(micropem)
 
 testthat::test_check("micropem")
