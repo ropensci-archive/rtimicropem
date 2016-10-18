9e4993c57c28e6350d77a58363176e9e34718608
masalmon <maelle.salmon@yahoo.se>
2016-10-18 14:17:50 +0200

    closes #22

---
+++ R/identify_lags.R
@@ -0,0 +1,24 @@
+#' Identify time gaps in the data collection period
+#'
+#' @param micropem a R6 micropem object
+#' @param column by default "rh_corrected_nephelometer",
+#' but could be "nephelometer", the column in which to look for gaps.
+#'
+#' @return A data.frame with all the rows of measures where the nephelometer measures is
+#' missing with a time to previous missing values smaller than the nephelometer log.
+#' @export
+#'
+#' @examples
+#' micropem_na <- convert_output(system.file("extdata", "file_with_na.csv",
+#'                                           package = "rtimicropem"))
+#' micropem_na$plot()
+#' identify_lags(micropem_na)
+#'
+identify_lags <- function(micropem){
+  data_na <- dplyr::filter_(micropem$measures,
+                            lazyeval::interp(~ is.na(rh_corrected_nephelometer)))
+
+  dplyr::filter_(data_na,
+                lazyeval::interp(~ difftime(datetime, lag(datetime),
+                                            units = "secs") < micropem$settings$nephelometerLogInterval))
+}
